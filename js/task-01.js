4c5d06454d4f05355c16fba799733f3e937332c1
DanTsvirkun <danila.tsvirkun555@gmail.com>
2023-08-31 10:54:25 +0300

    .

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-const categoriesList = document.querySelector('#categories');
-console.log(`Number of categories:`, categoriesList.children.length);
-
-[...categoriesList.children].forEach(elem => {
-    console.log(`Category: ${elem.firstElementChild.textContent}`);
-    console.log(`Elements:`, elem.lastElementChild.children.length);
-});
